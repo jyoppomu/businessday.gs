d9903e48a2eb9c2d3f9ee666bd9e051225a0ebad
jyoppomu <jyoppomu@gmail.com>
2026-02-05 16:04:55 +0900

    chore(fix): correct import plugin name in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,7 +30,7 @@ export default defineConfig([
 
     plugins: {
       "@typescript-eslint": typescriptEslint,
-      import: importPlugin,
+      "import": importPlugin,
     },
 
     settings: {
